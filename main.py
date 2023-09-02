2c1dca3a984d61c8a6c378ca29836062e66cc797
Dkalea <143872232+Dkalea@users.noreply.github.com>
2023-09-02 16:26:22 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+kata_gaul = {
+    "mager": "males gerak",
+    "gercep": "gerak cepet",
+    "gabut": "Gak ada kerjaan",
+    "japri": "jaringan pribadi",
+    "mantul": "mantap betul"
+}
+
+for i in range(5):
+    word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kecil semua!): ")
+    
+    if word in kata_gaul.keys():
+        # Apa yang harus kita lakukan jika kata itu ditemukan?
+        print('artinya adalah',kata_gaul[word])
+    else:
+        # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
+        print('kata tersebut tidak ditemukan')
